c87228655a9121bfc1299d27305141733ff97978
Murray Rogers <muzz@acemuzzy.com>
2019-12-08 18:03:36 +0000

    Tidy up the code somewhat!

--- src/main.rs
+++ src/main.rs
@@ -1,177 +1,160 @@
-const ADD : u32 = 1;
-const MULTIPLY : u32 = 2;
-const INPUT : u32 = 3;
-const OUTPUT : u32 = 4;
-const JUMPIFNZ : u32 = 5;
-const JUMPIFZ : u32 = 6;
-const JUMPLT : u32 = 7;
-const JUMPEQ : u32 = 8;
-const EXIT : u32 = 99;
+#[derive(Debug)]
+enum OpCode {
+    ADD,
+    MULTIPLY,
+    INPUT,
+    OUTPUT,
+    JUMPIFNZ,
+    JUMPIFZ,
+    JUMPLT,
+    JUMPEQ,
+    EXIT
+}
 
-const DIRECT : char = '1';
-const INDIRECT : char = '0';
+impl OpCode {
+    fn from(chars: &mut Vec<char>) -> Self {
 
-fn run(mut memory: Vec<i64>, input: i64) -> i64 {
-    let mut index = 0;
+        let opcode = chars.pop().unwrap().to_digit(10).unwrap() + (chars.pop().unwrap_or('0').to_digit(10).unwrap()) * 10;
 
-    loop {
-        let clone = memory.clone();
+        match opcode {
+            1 => OpCode::ADD,
+            2 => OpCode::MULTIPLY,
+            3 => OpCode::INPUT,
+            4 => OpCode::OUTPUT,
+            5 => OpCode::JUMPIFNZ,
+            6 => OpCode::JUMPIFZ,
+            7 => OpCode::JUMPLT,
+            8 => OpCode::JUMPEQ,
+            99 => OpCode::EXIT,
+            x => panic!("Unrecognized opcode {}", x)
+        }
+    }
+}
 
-        let instruction = format!("{}", memory[index]);
-        let mut instruction_chars : Vec<char> = instruction.chars().collect();
-        let opcode = instruction_chars.pop().unwrap().to_digit(10).unwrap() + (instruction_chars.pop().unwrap_or('0').to_digit(10).unwrap()) * 10;
-        print!("opcode {}:  ", opcode);
-        let jump = match opcode {
-            ADD => {
-                // add
-                let v1 = match instruction_chars.pop().unwrap_or('0') {
-                    INDIRECT => clone[clone[index + 1] as usize],
-                    _ => clone[index + 1]
-                };
-                let v2 = match instruction_chars.pop().unwrap_or('0') {
-                    INDIRECT => clone[clone[index + 2] as usize],
-                    _ => clone[index + 2]
-                };
-                println!("{} + {} -> {}", v1, v2, clone[index + 3]);
-                memory[clone[index + 3] as usize] = v1 + v2;
-                4
-            },
-            MULTIPLY => {
-                // multiply
-                let v1 = match instruction_chars.pop().unwrap_or('0') {
-                    INDIRECT => clone[clone[index + 1] as usize],
-                    _ => clone[index + 1]
-                };
-                let v2 = match instruction_chars.pop().unwrap_or('0') {
-                    INDIRECT => clone[clone[index + 2] as usize],
-                    _ => clone[index + 2]
-                };
-                println!("{} * {} -> {}", v1, v2, clone[index + 3]);
-                memory[clone[index + 3] as usize] = v1 * v2;
-                4
-            },
-            INPUT => {
-                println!("Input {}", input);
-                memory[clone[index + 1] as usize] = input;
-                2
-            },
-            OUTPUT => {
-                // output
-                println!("Output");
-                let v1 = match instruction_chars.pop().unwrap_or('0') {
-                    INDIRECT => clone[clone[index + 1] as usize],
-                    _ => clone[index + 1]
-                };
+#[derive(Debug)]
+enum ReferenceType {
+    DIRECT,
+    INDIRECT,
+}
 
-                if v1 != 0 {
-                    if clone[index + 2] as u32 == EXIT {
-                        println!("Output! {}", v1);
-                        return v1;
-                    } else {
-                        panic!("Diagnostic error!");
-                    }
-                }
-                2
-            },
-            JUMPIFZ => {
-                println!("Jump?");
-                let v1 = match instruction_chars.pop().unwrap_or('0') {
-                    INDIRECT => clone[clone[index + 1] as usize],
-                    _ => clone[index + 1]
-                };
-                let v2 = match instruction_chars.pop().unwrap_or('0') {
-                    INDIRECT => clone[clone[index + 2] as usize],
-                    _ => clone[index + 2]
-                };
+impl ReferenceType {
+    fn from(c: char) -> Self {
 
-                if v1 == 0 {
-                    println!("Jump!");
-                    index = v2 as usize;
-                    0
-                } else {
-                    println!("No");
-                    3
-                }
-            },
-            JUMPIFNZ => {
-                println!("Jump??");
-                let v1 = match instruction_chars.pop().unwrap_or('0') {
-                    INDIRECT => clone[clone[index + 1] as usize],
-                    _ => clone[index + 1]
-                };
-                let v2 = match instruction_chars.pop().unwrap_or('0') {
-                    INDIRECT => clone[clone[index + 2] as usize],
-                    _ => clone[index + 2]
-                };
+        match c {
+            '1' => ReferenceType::DIRECT,
+            '0' => ReferenceType::INDIRECT,
+            x => panic!("Unrecognized reference type {}", x)
+        }
+    }
+}
 
-                if v1 != 0 {
-                    println!("Jump!");
-                    index = v2 as usize;
-                    0
-                } else {
-                    println!("No");
-                    3
-                }
-            },
-            JUMPLT => {
-                println!("LT?");
-                let v1 = match instruction_chars.pop().unwrap_or('0') {
-                    INDIRECT => clone[clone[index + 1] as usize],
-                    _ => clone[index + 1]
-                };
-                let v2 = match instruction_chars.pop().unwrap_or('0') {
-                    INDIRECT => clone[clone[index + 2] as usize],
-                    _ => clone[index + 2]
-                };
+#[derive(Default)]
+struct Computer {
+    memory: Vec<i64>,
+    index: usize,
+    instruction_chars: Vec<char>,
+}
 
-                let outputval = if v1 < v2 {                    
-                    println!("Yes!");
-                    1
-                } else {
-                    println!("No!");
-                    0
-                };
+impl Computer {
 
-                memory[clone[index + 3] as usize] = outputval;
-                4
-            },
-            JUMPEQ => {
-                println!("Equal?");
-                let v1 = match instruction_chars.pop().unwrap_or('0') {
-                    INDIRECT => clone[clone[index + 1] as usize],
-                    _ => clone[index + 1]
-                };
-                let v2 = match instruction_chars.pop().unwrap_or('0') {
-                    INDIRECT => clone[clone[index + 2] as usize],
-                    _ => clone[index + 2]
-                };
-
-                let outputval = if v1 == v2 {
-                    println!("Yes!");             
-                    1
-                } else {
-                    println!("No!");
-                    0
-                };
-
-                memory[clone[index + 3] as usize] = outputval;
-                4
-            },                                    
-            EXIT => {
-                break;
-            },
-            x => {
-                panic!("WTF! Value {} in memory {:?}", x, memory);
-            }
-        };
-
-        index += jump;
+    fn new_from_file(filename: &str) -> Self {
+        Computer {
+            memory: std::fs::read_to_string(filename).unwrap().split(",").map(|input| input.clone().parse::<i64>().unwrap()).collect(),
+            ..Default::default()
+        }
     }
 
-    memory[0]
+    fn get_next_value(&mut self) -> i64 {
+        let val = match ReferenceType::from(self.instruction_chars.pop().unwrap_or('0')) {
+            ReferenceType::INDIRECT => self.memory[self.memory[self.index] as usize],
+            ReferenceType::DIRECT => self.memory[self.index],
+        };
+        self.index += 1;
+        val
+    }
+
+    fn write(&mut self, val: i64) {
+        let dest = self.memory[self.index] as usize;
+        self.memory[dest] = val;
+        self.index += 1;
+    }
+
+    fn get_instruction(&mut self) -> Vec<char> {
+        let instruction = self.memory[self.index].to_string().chars().collect();
+        self.index += 1;
+        instruction
+    }
+
+    fn run(mut self, input: i64) -> i64 {
+        loop {
+            self.instruction_chars = self.get_instruction();            
+            let opcode = OpCode::from(&mut self.instruction_chars);
+            println!("{:?}", opcode);
+
+            match opcode {
+                OpCode::ADD => {
+                    let val = self.get_next_value() + self.get_next_value();
+                    self.write(val);
+                },
+                OpCode::MULTIPLY => {
+                    let val = self.get_next_value() * self.get_next_value();
+                    self.write(val);
+                },
+                OpCode::INPUT => {
+                    self.write(input);
+                },
+                OpCode::OUTPUT => {
+                    let v1 = self.get_next_value();
+                    if v1 != 0 {
+                        return v1;
+                    }
+                },
+                OpCode::JUMPIFZ => {
+                    let (v1, v2) = (self.get_next_value(), self.get_next_value());
+                    if v1 == 0 {
+                        println!("Jump!");
+                        self.index = v2 as usize;
+                    }
+                },
+                OpCode::JUMPIFNZ => {
+                    let (v1, v2) = (self.get_next_value(), self.get_next_value());                    
+                    if v1 != 0 {
+                        println!("Jump!");
+                        self.index = v2 as usize;
+                    }
+                },
+                OpCode::JUMPLT => {
+                    let (v1, v2) = (self.get_next_value(), self.get_next_value());
+                    self.write(if v1 < v2 {                    
+                        1
+                    } else {
+                        0
+                    });
+                },
+                OpCode::JUMPEQ => {
+                    let (v1, v2) = (self.get_next_value(), self.get_next_value());
+                    self.write(if v1 == v2 {
+                        1
+                    } else {
+                        0
+                    });
+                },                                    
+                OpCode::EXIT => {
+                    break;
+                },
+            };
+        }
+
+        self.memory[0]
+    }
 }
 
 fn main() {
-    let memory : Vec<i64> = std::fs::read_to_string("inputs.txt").unwrap().split(",").map(|input| input.clone().parse::<i64>().unwrap()).collect();
-    print!("{}", run(memory, 5));
+    let computer = Computer::new_from_file("inputs.txt");
+
+    let mut args = std::env::args();
+    let _ = args.next();
+    let a1 = args.next().unwrap();
+
+    println!("{}", computer.run(a1.parse().unwrap()));
 }
